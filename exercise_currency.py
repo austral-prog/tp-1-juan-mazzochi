7bcfced541f4eb6cf90cfe11fb8ce3661c2b334e
juan-mazzochi <juanmartinmazzochi@gmail.com>
2026-03-13 09:18:39 -0300

    Update exercise_currency.py

--- exercise_currency.py
+++ exercise_currency.py
@@ -11,3 +11,9 @@ def currency():
     tasa_dolar = 1500  # 1 dólar = 1500 pesos
     tasa_euro = 1600   # 1 euro = 1600 pesos
     tasa_real = 250    # 1 real = 250 pesos
+    dolares = (pesos / 1500)
+    euros = (pesos / 1600)
+    brasileños = (pesos / 250)
+    print(dolares)
+    print(euros)
+    print(brasileños)
