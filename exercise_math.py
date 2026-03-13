6d1d412eda12f452e9c0336ca182c7419819e533
juan-mazzochi <juanmartinmazzochi@gmail.com>
2026-03-13 09:22:05 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -13,3 +13,18 @@ def math():
     """
     a = 57
     b = 7
+    suma = a + b
+    diferencia = a - b
+    producto = a * b
+    promedio = suma / 2
+    cociente_entero = a // b
+    resto = a % b
+    valor_real = a / b
+    print(suma)
+    print(diferencia)
+    print(producto)
+    print(promedio)
+    print(cociente_entero)
+    print(resto)
+    print(valor_real)
+    
